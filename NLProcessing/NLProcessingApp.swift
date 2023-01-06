22d4fb41d1569d560b43f604685eedd913276926
Jacek Kosiński <kashiash@gmail.com>
2023-01-06 12:41:16 +0100

    Initial Commit

---
+++ NLProcessing/NLProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLProcessingApp.swift
+//  NLProcessing
+//
+//  Created by Jacek Kosinski U on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NLProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
